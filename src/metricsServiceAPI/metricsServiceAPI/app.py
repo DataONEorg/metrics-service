1f889f4d28600c546ba538b55b7580c53adfe61e
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-04-19 11:25:57 -0700

    Falcon app

--- src/metricsServiceAPI/metricsServiceAPI/app.py
+++ src/metricsServiceAPI/metricsServiceAPI/app.py
@@ -1,8 +1,29 @@
+"""
+This code creates your WSGI application and aliases it as api.
+
+We use `application` variable name since that is what Gunicorn,
+by default, expects it to be called!
+
+We create unique resource and map to its endpoint.
+"""
+
+
 import falcon
 
-from .getMetrics import GetMetrics
+from .getmetrics import GetMetrics
+from .postmetricsfilter import PostMetricsFilters
 
-api = application = falcon.API()
+api = application = falcon.API() # pylint: disable=invalid-name
 
-getmetrics = GetMetrics()
-api.add_route('/getMetrics', getmetrics)
+# Creating a unique GET resource from the GetMetrics class
+getmetricsrequest = GetMetrics() # pylint: disable=invalid-name
+
+# Mapping the HTTP GET endpoint with its unique resource
+api.add_route('/getMetrics', getmetricsrequest)
+
+
+# Creating a unique POST resource from the PostMetricsFilters class
+postmetricsrequest = PostMetricsFilters() # pylint: disable=invalid-name
+
+# Mapping the HTTP POST endpoint with its unique resource
+api.add_route('/metrics/filter', postmetricsrequest)
