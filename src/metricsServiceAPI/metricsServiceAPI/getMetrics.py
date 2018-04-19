909708e9f43d2026d8e8abffe301dd5b8c465517
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-04-19 11:25:02 -0700

    getMetrics class associated with the GET endpoint

--- src/metricsServiceAPI/metricsServiceAPI/getMetrics.py
+++ src/metricsServiceAPI/metricsServiceAPI/getMetrics.py
@@ -1,16 +1,33 @@
-import json
+"""
+GetMetrics Module of the API. Handles the GeET Requests.
+"""
 
+import json
 import falcon
 
+from .metricshandler import MetricsHandler
+
+
+
 class GetMetrics(object):
+    """
+    This class is used to create a GET Resource
+    and map it with the GET endpoint of the API
+    """
 
-    def on_get(selfself, req, resp):
-        doc = {
-            'message': 'Hello Metrics!'
-        }
+    def on_get(self, req, resp):
+        """
+        The method assigned to the post end point
+        :param req: HTTP Request object
+        :param resp: HTTP Response object
+        :return: HTTP Response object
+        """
 
-        # Create a JSON representation of the resource
-        resp.body = json.dumps(doc, ensure_ascii=False)
+        metrics_request = json.loads(req.get_header('metricsRequest'))
+
+        metrics_handler = MetricsHandler(metrics_request)
+
+        resp.body = json.dumps(metrics_handler.process_request(), ensure_ascii=False)
 
         # The following line can be omitted because 200 is the default
         # status returned by the framework, but it is included here to
