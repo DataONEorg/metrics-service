16bda3bc8b6df422e2055d62220896aea3cdb188
Matt Jones <gitcode@magisa.org>
2018-04-26 16:23:44 -0800

    Add upport to mount the sql folder as a volume in docker.
    Now the tables, indices, and views get created on docker-compose up.

--- src/sql/00-metrics-tables.sql
+++ src/sql/00-metrics-tables.sql
@@ -7,7 +7,7 @@
  */
 CREATE SEQUENCE db_version_id_seq;
 CREATE TABLE db_version (
-  db_version_id   INT8 default nextval ('db_version_id_seq'), -- the identifier for the version
+  db_version_id   SERIAL,           -- the identifier for the version
   version         VARCHAR(250),     -- the version number
   status          INT8,             -- status of the version, 1 if its the current version
   CONSTRAINT db_version_pk PRIMARY KEY (db_version_id)
