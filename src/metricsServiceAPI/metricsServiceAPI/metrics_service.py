726cf5f43bbc1fb17bcc214ecceace00c36676c1
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-04-30 16:35:43 -0700

    Refactoring

--- src/metricsServiceAPI/metricsServiceAPI/metrics_service.py
+++ src/metricsServiceAPI/metricsServiceAPI/metrics_service.py
@@ -10,15 +10,15 @@ We create unique resource and map to its endpoint.
 
 import falcon
 
-from .MetricsReader import MetricsReader
+from .metrics_handler import MetricsHandler
 
 api = application = falcon.API() # pylint: disable=invalid-name
 
 # Creating a unique GET resource from the GetMetrics class
-metrics_Reader_resource = MetricsReader() # pylint: disable=invalid-name
+metrics_handler_resource = MetricsHandler() # pylint: disable=invalid-name
 
 # Mapping the HTTP GET endpoint with its unique resource
-api.add_route('/metrics', metrics_Reader_resource)
+api.add_route('/metrics', metrics_handler_resource)
 
 # Mapping the HTTP POST endpoint with its unique resource
-api.add_route('/metrics/filter', metrics_Reader_resource)
+api.add_route('/metrics/filter', metrics_handler_resource)
