d9fdfa5fb4bb1d37f7606689a7e59899c7b25b69
datadavev <datadavev@users.noreply.github.com>
2018-05-16 15:11:49 -0400

    Added comments in common

--- src/d1_metrics/d1_metrics/common.py
+++ src/d1_metrics/d1_metrics/common.py
@@ -5,9 +5,22 @@ import logging
 import dateparser
 from pytz import timezone
 
+#Default location of configuration file
 DEFAULT_CONFIG_FILE="/etc/dataone/metrics/database.ini"
 
+
 def textToDateTime(txt, default_tz='UTC'):
+  '''
+  Convert plain text to a timezone aware datetime instance.
+
+  e.g.: "now", "yesterday", "1 year ago", "10 days from now", "2:30pm"
+  Args:
+    txt: Textual representation of a dateTime
+    default_tz: Timezone to use when it can't be figured out.
+
+  Returns:
+    Timezone aware instance of DateTime
+  '''
   logger = logging.getLogger('common')
   d = dateparser.parse(txt, settings={'RETURN_AS_TIMEZONE_AWARE': True})
   if d is None:
