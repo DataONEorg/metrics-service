80e2b9a2d66d14d9e412e64b08a630ea4da81c0f
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-05-30 13:20:35 -0700

    HTTP get endpoing query param

--- src/d1_metrics_service/d1_metrics_service/metricsreader.py
+++ src/d1_metrics_service/d1_metrics_service/metricsreader.py
@@ -3,8 +3,9 @@ Metrics Reader module
 """
 import json
 import falcon
+from urllib.parse import urlparse
 
-from d1_metrics.d1_metrics.metricsdatabase import MetricsDatabase
+from d1_metrics.metricsdatabase import MetricsDatabase
 
 
 class MetricsReader:
@@ -24,7 +25,14 @@ class MetricsReader:
         :return: HTTP Response object
         """
 
-        metrics_request = json.loads(req.get_header('metricsRequest'))
+        #taking query parametrs from the HTTP GET request and forming metricsRequest Object
+        metrics_request = {}
+        query_param = urlparse(req.url)
+
+        for i in query_param.query.split("&"):
+            query = i.split(":")
+            metrics_request[query[0]] = ":".join(query[1:])
+
 
         resp.body = json.dumps(self.process_request(metrics_request), ensure_ascii=False)
 
