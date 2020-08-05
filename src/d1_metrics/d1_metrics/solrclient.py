bdae4718af7c3d34663df00e88f30eb2c15dd7a6
Rushiraj Nenuji <nenuji@nceas.ucsb.edu>
2020-08-05 07:06:51 -0700

    Fixing versions and path
    Fixing versions and path

--- src/d1_metrics/d1_metrics/solrclient.py
+++ src/d1_metrics/d1_metrics/solrclient.py
@@ -43,7 +43,7 @@ class SolrClient(object):
     self.logger = logging.getLogger(APP_LOG)
     self.client = requests.Session()
     self.parser = ConfigParser()
-    self.parser.read('/Users/rushirajnenuji/Documents/Github/metrics-service/localconfig.ini')
+    self.parser.read('/var/local/metrics-service/localconfig.ini')
     self.cert = None
     if(self.parser.get("solr_config", "load_certs") == "True"):
       self.cert = self._getSolrCert()
